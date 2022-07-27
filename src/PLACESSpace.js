abc9e34e87509f4f3e390296e4cfae60c09e03cf
altayozyavas <altayozyavas@gmail.com>
2022-07-27 15:52:38 -0500

    add content with content components

---
+++ src/PLACESSpace.js
@@ -0,0 +1,19 @@
+// src/PLACESSpace.js
+import logo from './logo.svg'
+
+function PLACESSpace(props) {
+  const { name, image, address } = props;
+  return (
+    <div>
+      <img src={`${process.env.PUBLIC_URL}/images/${image}`}     
+        width="300"
+        height="300"
+        alt="Buckingham Fountain is a Chicago Landmark"
+      />
+      <h1>{name}</h1>
+      <div>{address}</div>
+    </div>
+  )
+}
+
+export default PLACESSpace;
